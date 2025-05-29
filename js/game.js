da34cff8dcf3d26972d5050da18983a80ab39d9a
Arsene Kabula <arsene.kabula@stu.ocsb.ca>
2025-05-29 14:36:15 +0000

    Pushing Changes

---
+++ js/game.js
@@ -0,0 +1,47 @@
+// This file is part of the  project
+// Copyright (C) 2025 
+// Made by Arsene
+// this is the phaser 3 configuration file
+
+/* global Phaser */
+
+//* Game scene */
+
+import SplashScene from './splashScene.js';
+import TitleScene from './titleScene.js';
+import MenuScene from './menuScene.js';
+import GameScene from './gameScene.js';
+
+const splashScene = new SplashScene();
+const titleScene = new TitleScene();
+const menuScene = new MenuScene()
+const gameScene = new GameScene
+
+const config = {
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    backgroundColor: 0xffffff,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+}
+
+const game = new Phaser.Game(config);
+
+
+//* Game scenes */
+game.scene.add('splashScene', splashScene);
+game.scene.add('titleScene', titleScene);
+game.scene.add('menuScene', menuScene);
+game.scene.add('gameScene', gameScene);
+
+//* game title */
+game.scene.start('splashScene');
